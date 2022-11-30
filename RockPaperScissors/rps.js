f5971bad23fef881499a6e9ab048f5b809ddcf4a
Michael Chan <chan.myko@gmail.com>
2022-11-29 21:31:52 -0500

    Rock Paper Scissors revisted

--- RockPaperScissors/rps.js
+++ RockPaperScissors/rps.js
@@ -19,7 +19,7 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")) 
     {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
 }
 
@@ -29,14 +29,49 @@ function playRound(playerSelection, computerSelection) {
 //     }
 // }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = "";
-        while (!choices.includes(playerChoice.toLowerCase())) {
-            playerChoice = prompt("Rock, paper or scissors?");
-        }
-        console.log(playRound(playerChoice, getComputerChoice()));
-    }
-}
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerChoice = "";
+//         while (!choices.includes(playerChoice.toLowerCase())) {
+//             playerChoice = prompt("Rock, paper or scissors?");
+//         }
+//         console.log(playRound(playerChoice, getComputerChoice()));
+//     }
+// }
 
-game();
+// game();
+
+const buttons = document.querySelectorAll('button');
+const resultDiv = document.querySelector('.results')
+
+let playerWins = 0;
+let computerWins = 0;
+
+buttons.forEach(btn => btn.addEventListener('click', () => {
+    let paragraph = document.createElement("p");
+    let result = playRound(btn.innerText, getComputerChoice());
+    
+    if (result.includes("win")) {
+        playerWins++;
+    } else if (result.includes("lose")) {
+        computerWins++;
+    }
+
+    result += ` Score: ${playerWins}-${computerWins}`
+    paragraph.textContent = result;
+    resultDiv.appendChild(paragraph);
+
+    if (playerWins >= 5) {
+        let winnerParagraph = document.createElement("p");
+        winnerParagraph.textContent = "You win! Resetting score to 0-0."
+        resultDiv.appendChild(winnerParagraph);
+        playerWins = 0;
+        computerWins = 0;
+    } else if (computerWins >= 5) {
+        let winnerParagraph = document.createElement("p");
+        winnerParagraph.textContent = "Computer wins! Resetting score to 0-0."
+        resultDiv.appendChild(winnerParagraph);
+        playerWins = 0;
+        computerWins = 0;
+    }
+}));
